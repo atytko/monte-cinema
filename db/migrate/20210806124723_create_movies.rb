598b74ffaa9cd21d6a300ffd736cdc0b575b8545
atytko <tytko.anna@gmail.com>
2021-08-06 14:11:20 +0100

    fix style offences

--- db/migrate/20210806124723_create_movies.rb
+++ db/migrate/20210806124723_create_movies.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string :title, null: false
       t.text :description, null: false
-      t.interval :duration, null: false
+      t.integer :duration, null: false
       t.string :genre, null: false
       t.string :director, null: false
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_114723) do
+ActiveRecord::Schema.define(version: 2021_08_06_124723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2021_08_06_114723) do
   create_table "movies", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
-    t.interval "duration", null: false
+    t.integer "duration", null: false
     t.string "genre", null: false
     t.string "director", null: false
     t.datetime "created_at", precision: 6, null: false
