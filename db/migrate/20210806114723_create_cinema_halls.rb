a5bb9f7ee020b76e9198b17c246139da9cc0385e
atytko <tytko.anna@gmail.com>
2021-08-09 12:10:34 +0100

    add devise and create user model

--- db/migrate/20210806114723_create_cinema_halls.rb
+++ db/migrate/20210806114723_create_cinema_halls.rb
@@ -4,7 +4,7 @@ class CreateCinemaHalls < ActiveRecord::Migration[6.1]
   def change
     create_table :cinema_halls do |t|
       t.string :name, null: false
-      t.string :row, null: false
+      t.integer :row_number, null: false
       t.integer :row_total_seats, null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_124723) do
+ActiveRecord::Schema.define(version: 2021_08_09_105027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cinema_halls", force: :cascade do |t|
     t.string "name", null: false
-    t.string "row", null: false
+    t.integer "row_number", null: false
     t.integer "row_total_seats", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -41,6 +41,32 @@ ActiveRecord::Schema.define(version: 2021_08_06_124723) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cinema_hall_id"], name: "index_screenings_on_cinema_hall_id"
     t.index ["movie_id"], name: "index_screenings_on_movie_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.json "tokens"
+    t.bigint "user_role_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+    t.index ["user_role_id"], name: "index_users_on_user_role_id"
   end
 
 end
